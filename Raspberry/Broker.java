d11b63b1df6b4e6dbf1e880c67f550e1c2873176
ornitor <othonrocha@gmail.com>
2019-11-03 11:32:28 -0300

    Add files via upload

---
+++ Raspberry/Broker.java
@@ -0,0 +1,11 @@
+
+/**
+ * Dados Privados
+ */
+public class Broker
+{
+    public static String user = "seu-user-broker";
+    public static String pass = "seu-pass-broker";
+    public static String url = "seu.mqttbroker.com";
+    public static String port = "que-porta";
+}
